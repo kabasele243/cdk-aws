9d7e7513cd4c668b2caba3047c7c4311e63a8460
Franck Kabasele <kabasele467@gmail.com>
2022-02-17 17:10:56 -0500

    setting up generic table for dynamo db

--- infrastructure/SpaceStack.ts
+++ infrastructure/SpaceStack.ts
@@ -3,11 +3,17 @@ import { Construct } from 'constructs';
 import { Function as LambdaFunction, Runtime, Code } from 'aws-cdk-lib/aws-lambda';
 import { join } from 'path';
 import { LambdaIntegration, RestApi} from 'aws-cdk-lib/aws-apigateway';
+import { GenericTable } from './GenericTable';
 
 
 export class SpaceStack extends Stack{
 
     private api = new RestApi(this, 'SpaceApi');
+    private spacesTable = new GenericTable(
+        'SpacesTable',
+        'spaceId',
+        this
+    )
 
     constructor(scope: Construct, id: string, props: StackProps) {
         super(scope, id, props)
