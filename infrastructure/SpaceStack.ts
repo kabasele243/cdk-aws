c05f421c0390111fc3371b65bb05617a39cd8e74
Franck Kabasele <kabasele467@gmail.com>
2022-02-17 16:33:13 -0500

    setting up our first lambda

--- infrastructure/SpaceStack.ts
+++ infrastructure/SpaceStack.ts
@@ -1,10 +1,18 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import { Function as LambdaFunction, Runtime, Code } from 'aws-cdk-lib/aws-lambda';
+import { join } from 'path';
 
 
 export class SpaceStack extends Stack{
 
     constructor(scope: Construct, id: string, props: StackProps) {
         super(scope, id, props)
+
+        const helloLamba = new LambdaFunction(this, 'helloLamba', {
+            runtime: Runtime.NODEJS_14_X,
+            code: Code.fromAsset(join(__dirname, '..', 'services')),
+            handler: 'hello.main'
+        })
     }
 }
